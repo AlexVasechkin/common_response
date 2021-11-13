f44ccdbbf726efc94ccdd06ec0abea91057764a1
Александр Васечкин <administrator@MacBook-Pro-Aleksandr.local>
2021-11-13 18:08:56 +0300

    Initial state

--- README.md
+++ README.md
@@ -1,1 +1,35 @@
-# common_response
+# PHP Class Response
+
+Class CommonHelpers\Response should use if you want to get boolean result of the operations and message. Also you have an opportunity get any data besides the boolean result and message. It is comfortable for api.
+
+```php
+<?php
+
+namespace App;
+
+use CommonHelpers\Response;
+
+class ApiController
+{
+    private $response;
+
+    public function __construct()
+    {
+        $this->response = Response::fail();
+    }
+
+    public function run(int $a, int $b): Response
+    {
+        $this->response->setResult($a > $b);
+
+        $this->response->isSuccess() ? $this->response->setMessage('ok')
+                                     : $this->response->setMessage('fail')
+        ;
+
+        $this->response->payload = $a * $b;
+
+        return $this->response;
+    }
+}
+
+```
---
+++ src/Response.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace CommonHelpers;
+
+class Response
+{
+    private $result;
+
+    private $message;
+
+    /** @var mixed|null $payload */
+    public $payload;
+
+    public function __construct(string $message = '', bool $result = false, $payload = null)
+    {
+        $this->setMessage($message);
+
+        $this->setResult($result);
+
+        $this->payload = $payload;
+    }
+
+    public static function success(string $message = '', $payload = null): Response
+    {
+        return new Response($message, true);
+    }
+
+    public static function fail(string $message = '', $payload = null): Response
+    {
+        return new Response($message, false, $payload);
+    }
+
+    public function setResult(bool $result): self
+    {
+        $this->result = $result;
+
+        return $this;
+    }
+
+    public function isSuccess(): bool
+    {
+        return $this->result;
+    }
+
+    public function setMessage(string $message): self
+    {
+        $this->message = $message;
+
+        return $this;
+    }
+
+    public function getMessage(): string
+    {
+        return $this->message;
+    }
+}
